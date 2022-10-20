826b19c4ad5da4539a6706a2696d0fdb97ba6899
Erick Oliveira <ericksilvaredes@gmail.com>
2022-10-20 16:14:38 -0300

    Nav bar feita

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import './index.scss'
 import Home from "./routes/home/home.component";
 import { Routes, Route } from "react-router-dom";
+import Navigation from "./routes/navigation/navigation.component";
+import Shop from "./routes/shop/shop.component";
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/home" element={<Home/>}/>
+      <Route path='/' element={<Navigation/>}>
+        <Route index element={<Home/>}/>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/shop" element={<Shop/>}/>
+      </Route>
     </Routes>
   )
 }
---
+++ src/routes/shop/shop.component.jsx
@@ -0,0 +1,9 @@
+
+
+const Shop = () => {
+  return (
+    <h4>I'm the Shop</h4>
+  )
+}
+
+export default Shop
