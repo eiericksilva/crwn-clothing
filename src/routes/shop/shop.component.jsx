c12589cb6db5ef36bf1a5d43aa482300c7fe1c6e
eiericksilva <ericksilvaredes@gmail.com>
2023-01-11 20:59:15 -0300

    app finished with scss

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,6 +1,6 @@
 import { useContext } from "react";
 import { CategoriesContext } from "../../contexts/categories.context";
-import CategoryPreview from "../../components/category-preview/category-preview.component";
+import Category from "../category/category.component";
 import { Routes, Route } from "react-router-dom";
 import "./shop.styles.scss";
 import CategoriesPreview from "../categories-preview/categories-preview.component";
@@ -9,6 +9,7 @@ const Shop = () => {
   return (
     <Routes>
       <Route index element={<CategoriesPreview />} />
+      <Route path=":category" element={<Category />} />
     </Routes>
   );
 };
