6a2d5301f419b87998a6078ab81d96ae71bd5539
eiericksilva <ericksilvaredes@gmail.com>
2023-01-12 12:50:45 -0300

    refactoring DirectoryItem styles

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,21 +1,21 @@
-import './cart-icon.styles.scss'
-import { useContext } from 'react'
-import { CartContext } from '../../contexts/cart.context'
-import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg'
+import { useContext } from "react";
 
-import React from 'react'
+import { CartContext } from "../../contexts/cart.context";
+
+import { ShoppingIcon, ItenCount, CartIconContainer } from "./cart-icon.styles";
+
+import React from "react";
 
 const CartIcon = () => {
-  const { isCartOpen, setIsCartOpen, cartItemCount } = useContext(CartContext)
-  const toggleIsCartOpen = () => setIsCartOpen(!isCartOpen)
-  
+  const { isCartOpen, setIsCartOpen, cartItemCount } = useContext(CartContext);
+  const toggleIsCartOpen = () => setIsCartOpen(!isCartOpen);
 
   return (
-    <div className='cart-icon-container' onClick={toggleIsCartOpen}>
-        <ShoppingIcon className='shopping-icon'/>
-        <span className='item-count'>{cartItemCount}</span>
-    </div>
-  )
-}
+    <CartIconContainer onClick={toggleIsCartOpen}>
+      <ShoppingIcon className="shopping-icon" />
+      <ItenCount>{cartItemCount}</ItenCount>
+    </CartIconContainer>
+  );
+};
 
-export default CartIcon
+export default CartIcon;
