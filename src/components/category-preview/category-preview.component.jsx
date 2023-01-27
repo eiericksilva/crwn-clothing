0b6801e7d94ea49c8ebf5108a60d02c18a05b382
eiericksilva <ericksilvaredes@gmail.com>
2023-01-27 15:23:36 -0300

    aplicando redux-thunk

--- src/components/category-preview/category-preview.component.jsx
+++ src/components/category-preview/category-preview.component.jsx
@@ -6,19 +6,28 @@ import {
   Preview,
 } from "./category-preview.styles";
 
+import Spinner from "../spinner/spinner.component";
+import { selectCategoriesIsLoading } from "../../redux/store/categories/category.selector";
+import { useSelector } from "react-redux";
+
 const CategoryPreview = ({ title, products }) => {
+  const isLoading = useSelector(selectCategoriesIsLoading);
   return (
     <CategoryPreviewContainer>
       <h2>
         <Title to={title}>{title.toUpperCase()}</Title>
       </h2>
-      <Preview>
-        {products
-          .filter((_, idx) => idx < 4)
-          .map((product) => (
-            <ProductCard key={product.id} product={product} />
-          ))}
-      </Preview>
+      {isLoading ? (
+        <Spinner />
+      ) : (
+        <Preview>
+          {products
+            .filter((_, idx) => idx < 4)
+            .map((product) => (
+              <ProductCard key={product.id} product={product} />
+            ))}
+        </Preview>
+      )}
     </CategoryPreviewContainer>
   );
 };
